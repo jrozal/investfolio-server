102ce6046bcc8c7bf48004936ec30f5def4123ad
jrozal <68182930+jrozal@users.noreply.github.com>
2021-11-17 19:28:15 -0800

    Change port and cors settings for Heroku

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const port = 3001;
+const port = process.env.PORT || 3001;
 const router = require('./routes');
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: '*' }));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
